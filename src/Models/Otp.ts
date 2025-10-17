fa537957e1a580376b58d5a0c3db34ae6ed0e224
AMIR-NIAZ <anatomy.atlas15@gmail.com>
2025-10-17 19:30:52 +0330

    feat(otp): add Model OTP

---
+++ src/Models/Otp.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const OtpSchema = new mongoose.Schema(
+    {
+        code: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        attempts: {
+            type: Number,
+            default: 0,
+        },
+        verified: {
+            type: Boolean,
+            default: false,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            expires: 15000,
+        },
+    },  
+);
+
+const Otp = mongoose.model("Otp", OtpSchema);
+export default Otp;
