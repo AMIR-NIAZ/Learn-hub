88a073f2675572f5ac6831f5a21a72445f3ece9a
AMIR-NIAZ <anatomy.atlas15@gmail.com>
2025-10-18 23:02:48 +0330

    feat(Ticket): add mehtod addTicketReply

--- src/Controllers/TicketController.ts
+++ src/Controllers/TicketController.ts
@@ -3,6 +3,8 @@ import { AppError } from "../Utils/AppError";
 import { TicketValidator } from "../Validators/TicketValidator";
 import Ticket from "../Models/Ticket";
 import { isValidObjectId } from "mongoose";
+import { sendReplyTicketEmail } from "../Utils/email";
+import { ITicket } from "../Interfaces/ITicket";
 
 export class TicketController {
     public static async Create(req: Request, res: Response) {
@@ -40,9 +42,23 @@ export class TicketController {
 
     public static async deleteTicket(req: Request, res: Response) {
         const { id } = req.params;
-        if (isValidObjectId(id)) throw new AppError("id is not valid", 404);
+        if (!isValidObjectId(id)) throw new AppError("id is not valid", 422);
         await Ticket.deleteOne({ _id: id })
         return res.status(200).json("Ticket deleted successfull");
+    }
+
+    public static async addTicketReply(req: Request, res: Response) {
+        const { body } = req.body;
+        const { id } = req.params;
+        if (!isValidObjectId(id)) throw new AppError("id is not valid", 422);
+        if (!body || body.length < 10) throw new AppError("Reply must be at least 10 characters", 422);
+        const ticket = await Ticket.findById(id)
+            .populate("user")
+            .lean<ITicket>();
+        if (!ticket) throw new AppError("ticket not find", 404);
+        await sendReplyTicketEmail(body, ticket);
+        await Ticket.updateOne({ _id: id }, { answer: true });
+        res.status(200).json({ success: true, message: "Reply sent successfully" });
     }
 
     private static async Validation(req: Request) {
