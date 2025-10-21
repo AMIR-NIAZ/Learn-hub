ffca27bfe51b757d7e20deb14689e199ed6a8e68
AMIR-NIAZ <anatomy.atlas15@gmail.com>
2025-10-22 00:38:57 +0330

    fix/(middelwer): create checkRole

--- src/Middlewares/AuthMiddleware.ts
+++ src/Middlewares/AuthMiddleware.ts
@@ -2,63 +2,26 @@ import { Request, Response, NextFunction } from "express";
 import { AppError } from "../Utils/AppError";
 import { verifyToken } from "../Utils/CreateToken";
 import User from "../Models/User";
+import { Role } from "../Enums/RoleEnum";
+import { isValidObjectId } from "mongoose";
 
 export class AuthMiddleware {
-    static async getUser(req: Request, res: Response, next: NextFunction) {
-        const authHeader = req.headers['authorization'];
-        if (typeof authHeader !== "string" || !authHeader.startsWith("Bearer ")) return next();
-        const token = authHeader.split(" ")[1]
-        if (!token) return next();
-        let tokenVrify = await verifyToken(token)
-        let user = await User.findById(tokenVrify.id).lean()
-        if (user) (req as any).user = user
-        next()
+    static checkRole(role: string) {
+        return async (req: Request, res: Response, next: NextFunction) => {
+            const authHeader = req.headers['authorization'];
+            if (typeof authHeader !== "string" || !authHeader.startsWith("Bearer ")) {
+                throw new AppError("token is not valid", 401);
+            }
+            const token = authHeader.split(" ")[1];
+            if (!token) throw new AppError("token is not valid", 401);
+            let tokenVrify = await verifyToken(token)
+            if (!role.split('|').includes(tokenVrify.role) && !role.split('|').includes(Role.ANY)) {
+                throw new AppError(`Access denied: ${role.split('|').join(", ")} only`, 403);
+            }
+            if (isValidObjectId(!tokenVrify.id)) throw new AppError("token is not valid", 401);
+            let user = await User.findById(tokenVrify.id).lean();
+            if (user) (req as any).user = user;
+            next()
+        }
     }
-
-
-    static async isUser(req: Request, res: Response, next: NextFunction) {
-        const authHeader = req.headers['authorization'];
-        if (typeof authHeader !== "string" || !authHeader.startsWith("Bearer ")) throw new AppError("token is not valid", 401);
-        const token = authHeader.split(" ")[1]
-        if (!token) throw new AppError("token is not valid", 401);
-        let tokenVrify = await verifyToken(token)
-        let user = await User.findById(tokenVrify.id).lean()
-        if (!user) throw new AppError("User not found", 404);
-        (req as any).user = user
-        next()
-    }
-
-    static async isAdmin(req: Request, res: Response, next: NextFunction) {
-        const user = (req as any).user;
-        if (!user) {
-            throw new AppError("User not authenticated", 401);
-        }
-        if (user.role !== "ADMIN") {
-            throw new AppError("Access denied: Admins only", 403);
-        }
-        next();
-    }
-
-    static async isTeacher(req: Request, res: Response, next: NextFunction) {
-        const user = (req as any).user;
-        if (!user) {
-            throw new AppError("User not authenticated", 401);
-        }
-        if (user.role !== "TEACHER") {
-            throw new AppError("Access denied: Teachers only", 403);
-        }
-        next();
-    }
-
-    static async isTeacherORAdmin(req: Request, res: Response, next: NextFunction) {
-        const user = (req as any).user;
-        if (!user) {
-            throw new AppError("User not authenticated", 401);
-        }
-        if (user.role !== "TEACHER" || user.role !== "TEACHER") {
-            throw new AppError("Access denied: Admins and Teacher only", 403);
-        }
-        next();
-    }
-
 }
